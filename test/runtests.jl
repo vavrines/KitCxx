1f72e87a389ed454267ff3e2fd27eff71356bf2b
vavrines <tianbaixiao@gmail.com>
2022-12-20 20:52:10 +0100

    Update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,9 @@
 #using KitCxx
-
 cd(@__DIR__)
 include("../src/KitCxx.jl")
 using .KitCxx
 
-KitCxx.greet()
-
-x = KC.World()
-
-KC.set(x, "hello")
-
-KC.greet(x)
-
-prim = zeros(3)
-w = [1.0, 0.0, 1.0]
-γ = 1.4
-
-KC.conserve_prim_1d(prim, w, γ)
-
-x = KC.Solution()
-KC.set(x, 1.0, 2.0)
-KC.get(x)
-
+prim = [1.0, 0.0, 1.0]
+u = -5:0.01:5 |> collect
+f = zero(u)
+KC.maxwellian_1f1v(f, u, prim)
